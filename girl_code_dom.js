aaebde165ed5afaf0a52054ca97c8dde646d5adc
Sesethu24 <seshmomoza20@gmail.com>
2019-08-04 03:48:22 +0200

    first commit

---
+++ girl_code_dom.js
@@ -0,0 +1,11 @@
+// const bookTypeOne = document.querySelector(".selectBook");
+// const buttonElem = document.querySelector(".signInbtn");
+// var logoutbtnElem = document.querySelector(".logout");
+// // const bookTypeTwo = document.querySelector(".selectBook2");
+// // const bookTypeThree = document.querySelector(".selectBook3");
+
+// function TheeBooks(){
+
+//     alert("Thank You")
+// }
+// logoutbtnElem.addEventListener('click', TheeBooks)
